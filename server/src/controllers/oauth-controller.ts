7c5731cb72989931e1c178277dd8530032877700
Gabriela Mendes Pereira Irmão <gabriela.mendes@C02SR23HH3QF.local>
2020-11-25 12:22:04 -0300

    controller oauth

--- server/src/controllers/oauth-controller.ts
+++ server/src/controllers/oauth-controller.ts
@@ -1,13 +1,28 @@
 import { Request, Response } from 'express';
-import nodeFetch from 'node-fetch';
+import fetch from 'node-fetch';
 export default class OauthController {
 
     private apiKey: string;
     private clienteId: string;
+    private oauthUrl = 'https://accounts.google.com/o/oauth2/token';
 
     constructor(apitKey: string, clienteId: string) {
         this.apiKey = apitKey;
         this.clienteId = clienteId;
+    }
+
+    public async create(req: Request, res: Response) {
+        fetch(this.oauthUrl, {
+            method: 'post',
+            headers: {
+                contentType: 'application/x-www-form-urlencoded',
+                code: '',
+                client_id: '',
+                client_secret: '',
+                // redirect_uri=http://localhost/oauth2callback&,
+                grant_type: 'authorization_code',
+            }
+        })
     }
 
 };
